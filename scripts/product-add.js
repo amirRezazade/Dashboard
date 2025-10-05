00bdc51d35dd06a2ee6f088a609e2a3d5d428568
amir <amirrezazadeh.job@gmail.com>
2025-10-05 12:45:16 +0330

    finish add/edit product page

--- scripts/product-add.js
+++ scripts/product-add.js
@@ -1,9 +1,6 @@
 import {
-  addToLocal,
-  getToLocal,
   showSwal,
   showTost,
-  addToShoppingCard,
 } from "./funcs.js";
 const urlParams = new URLSearchParams(window.location.search);
 const productId = urlParams.get("id");
@@ -24,40 +21,34 @@ const shipping = document.querySelector("#shipping");
 const warranty = document.querySelector("#warranty");
 const returnElem = document.querySelector("#return");
 const productThumbnail = document.querySelector("#product-thumbnail");
-const submitBtn = document.querySelector('#submit-btn')
-const cancelBtn = document.querySelector('#cancel-btn')
-const requiredInputs = document.querySelectorAll('input[required]');
-
-
+const submitBtn = document.querySelector("#submit-btn");
+const cancelBtn = document.querySelector("#cancel-btn");
+const requiredInputs = document.querySelectorAll("input[required]");
 
 let editorValue;
-      ClassicEditor
-  .create(document.querySelector('#editor'), {
-    language: {
-      ui: 'fa',      
-      content: 'fa'    
-    }
-  })
-  .then(editor => {
-   editorValue = editor
-    editor.editing.view.change(writer => {
-      writer.setAttribute('dir', 'rtl', editor.editing.view.document.getRoot());
-      
+ClassicEditor.create(document.querySelector("#editor"), {
+  language: {
+    ui: "fa",
+    content: "fa",
+  },
+})
+  .then((editor) => {
+    editorValue = editor;
+    editor.editing.view.change((writer) => {
+      writer.setAttribute("dir", "rtl", editor.editing.view.document.getRoot());
     });
   })
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
   });
 if (productId) {
   getItem();
-  submitBtn.textContent = 'ویرایش محصول'
-  cancelBtn.textContent = 'حذف محصول'
+  submitBtn.textContent = "ویرایش محصول";
+  cancelBtn.textContent = "حذف محصول";
 }
 async function getItem() {
   let res = await fetch(`https://dummyjson.com/products/${productId}`);
   let product = await res.json();
-  console.log(product);
-
   name.value = product.title || "";
   sku.value = product.sku || "";
   category.value = product.category || "all";
@@ -69,70 +60,74 @@ async function getItem() {
   price.value = product.price || "";
   stock.value = product.stock || 0;
   discount.value = product.discountPercentage || 0;
-  discount.nextElementSibling.textContent = product.discountPercentage + '%' || 0 + '%';
+  discount.nextElementSibling.textContent =
+    product.discountPercentage + "%" || 0 + "%";
   shipping.value = product.shippingInformation || "all";
   warranty.value = product.warrantyInformation || "No warranty";
   returnElem.value = product.returnPolicy || "No return policy";
-  editorValue.setData(`${product.description}`) || '';
-  product.tags.forEach(tag => tagsContainer.innerHTML+= ` <div class="flex items-center  divide-x divide-gray-300 bg-[var(--active-color)] text-white  text-xs overflow-hidden rounded-r-full"><span class="p-2">${tag}</span> <button type="button" class="p-2 cursor-pointer centered" onclick="parentElement.remove()">X</button></div>` || '');
- 
+  editorValue.setData(`${product.description}`) || "";
+  product.tags.forEach(
+    (tag) =>
+      (tagsContainer.innerHTML +=
+        ` <div class="flex items-center  divide-x divide-gray-300 bg-[var(--active-color)] text-white  text-xs overflow-hidden rounded-r-full"><span class="p-2">${tag}</span> <button type="button" class="p-2 cursor-pointer centered" onclick="parentElement.remove()">X</button></div>` ||
+        "")
+  );
+
   const thumbnailImage = {
-  name: 'product.jpg',
-  size: 0.7,
-  type: "image/jpeg",
-  url: product.thumbnail
-};
-thumbnail.emit("addedfile", thumbnailImage);
-thumbnail.emit("thumbnail", thumbnailImage, thumbnailImage.url);
-thumbnail.emit("complete", thumbnailImage);
- 
-product.images.forEach(img=>{
+    name: "product.jpg",
+    size: 0.7,
+    type: "image/jpeg",
+    url: product.thumbnail,
+    accepted: true,
+  };
+  thumbnail.emit("addedfile", thumbnailImage);
+  thumbnail.emit("thumbnail", thumbnailImage, thumbnailImage.url);
+  thumbnail.emit("complete", thumbnailImage);
+  thumbnail.files.push(thumbnailImage);
+  product.images.forEach((img) => {
     const itemImage = {
-  name: 'product.jpg',
-  size: 0.7,
-  type: "image/jpeg",
-  url: img
-};
-gallery.emit("addedfile", itemImage);
-gallery.emit("thumbnail", itemImage, itemImage.url);
-gallery.emit("complete", itemImage);
-})
-
-
+      name: "product.jpg",
+      size: 0.7,
+      type: "image/jpeg",
+      url: img,
+      accepted: true,
+    };
+    gallery.emit("addedfile", itemImage);
+    gallery.emit("thumbnail", itemImage, itemImage.url);
+    gallery.emit("complete", itemImage);
+    gallery.files.push(itemImage);
+  });
 }
 
-tags.addEventListener('keyup' , e=>{
-  if(e.code=== 'Enter' && tags.value.trim()  ){
-    if(tagsContainer.childElementCount < 8){
-        tagsContainer.nextElementSibling.classList.add('hidden')
-        tagsContainer.innerHTML+=`
-         <div class="flex items-center  divide-x divide-gray-300 bg-[var(--active-color)] text-white  text-xs overflow-hidden rounded-r-full"><span class="p-2">${tags.value.trim()}</span> <button type="button" class="p-2 cursor-pointer centered" onclick="parentElement.remove()">X</button></div>`
-         tags.value=''
-         console.log(tagsContainer.childElementCount);
-
-    }else tagsContainer.nextElementSibling.classList.remove('hidden')
+tags.addEventListener("keyup", (e) => {
+  if (e.code === "Enter" && tags.value.trim()) {
+    if (tagsContainer.childElementCount < 8) {
+      tagsContainer.nextElementSibling.classList.add("hidden");
+      tagsContainer.innerHTML += `
+         <div class="flex items-center  divide-x divide-gray-300 bg-[var(--active-color)] text-white  text-xs overflow-hidden rounded-r-full"><span class="p-2">${tags.value.trim()}</span> <button type="button" class="p-2 cursor-pointer centered" onclick="parentElement.remove()">X</button></div>`;
+      tags.value = "";
+    } else tagsContainer.nextElementSibling.classList.remove("hidden");
   }
-  
-})
+});
 
-// start dropzone library 
+// start dropzone library
 Dropzone.autoDiscover = false;
 const gallery = new Dropzone("#product-gallery", {
-  url: "#", 
+  url: "#",
   autoProcessQueue: false,
   maxFiles: 5,
   maxFilesize: 1,
   acceptedFiles: "image/*",
-  dictDefaultMessage: "📸 عکس های محصول را اینجا بکشید یا کلیک کنید برای انتخاب",
+  dictDefaultMessage:
+    "📸 عکس های محصول را اینجا بکشید یا کلیک کنید برای انتخاب",
   addRemoveLinks: true,
   dictRemoveFile: "حذف تصویر",
   dictFileTooBig: "حجم تصویر زیاد است (حداکثر: {{maxFilesize}}MB)",
-
 });
 
 Dropzone.autoDiscover = false;
 const thumbnail = new Dropzone("#product-thumbnail", {
-  url: "#", 
+  url: "#",
   autoProcessQueue: false,
   maxFiles: 1,
   maxFilesize: 1,
@@ -141,93 +136,163 @@ const thumbnail = new Dropzone("#product-thumbnail", {
   addRemoveLinks: true,
   dictRemoveFile: "حذف تصویر",
   dictFileTooBig: "حجم تصویر زیاد است (حداکثر: {{maxFilesize}}MB)",
-
 });
 
-// finish dropzone library 
+// finish dropzone library
 
+submitBtn.addEventListener("click", () => {
+  if (formValidation()) submitProduct();
+});
 
-
-submitBtn.addEventListener('click' , ()=>{
-formValidation() 
-
-
-})
-
-
-function formValidation(){
-  if(price.value.length==0 || price.value <1) {
-    price.parentElement.nextElementSibling.classList.remove('hidden')
-    price.parentElement.classList.replace('border-transparent' , 'border-red-700')
+function formValidation() {
+  let isValid = true;
+  if (price.value.length == 0 || price.value < 1) {
+    price.parentElement.nextElementSibling.classList.remove("hidden");
+    price.parentElement.classList.replace(
+      "border-transparent",
+      "border-red-700"
+    );
+    isValid = false;
+  } else {
+    price.parentElement.nextElementSibling.classList.add("hidden");
+    price.parentElement.classList.replace(
+      "border-red-700",
+      "border-transparent"
+    );
   }
-  else{
-    price.parentElement.nextElementSibling.classList.add('hidden')
-    price.parentElement.classList.replace( 'border-red-700' , 'border-transparent')
-  }
-requiredInputs.forEach(elem=>{
-  if(!elem.checkValidity() ) {
-   if(elem.nextElementSibling) {
-     elem.nextElementSibling.classList.remove('hidden')
-     elem.classList.replace('border-transparent' , 'border-red-700')
+  requiredInputs.forEach((elem) => {
+    if (!elem.checkValidity()) {
+      if (elem.nextElementSibling) {
+        elem.nextElementSibling.classList.remove("hidden");
+        elem.classList.replace("border-transparent", "border-red-700");
+        isValid = false;
+      }
+    } else {
+      if (elem.nextElementSibling) {
+        elem.nextElementSibling.classList.add("hidden");
+        elem.classList.replace("border-red-700", "border-transparent");
+      }
     }
-  }else{
-     if(elem.nextElementSibling) {
-     elem.nextElementSibling.classList.add('hidden')
-      elem.classList.replace( 'border-red-700' ,'border-transparent' )
-    }
-  }
-})
+  });
 
-  document.querySelectorAll('select[required]').forEach(select=>{
-    if(select.value==='all') {
-      select.nextElementSibling.classList.remove('hidden') 
-      select.classList.add('border-red-600')
-    }else{
-      select.nextElementSibling.classList.add('hidden') 
-      select.classList.remove('border-red-600')
-
+  document.querySelectorAll("select[required]").forEach((select) => {
+    if (select.value === "all") {
+      select.nextElementSibling.classList.remove("hidden");
+      select.classList.add("border-red-600");
+      isValid = false;
+    } else {
+      select.nextElementSibling.classList.add("hidden");
+      select.classList.remove("border-red-600");
     }
-  })
-  if(editorValue.getData()=== ''){
-    document.querySelector('.ck-editor').style.border='1px solid red'
-    document.querySelector('#editor-alert').classList.remove('hidden')
-  }else{
-    document.querySelector('.ck-editor').style.border=''
-    document.querySelector('#editor-alert').classList.add('hidden')
+  });
+  if (editorValue.getData() === "") {
+    document.querySelector(".ck-editor").style.border = "1px solid red";
+    document.querySelector("#editor-alert").classList.remove("hidden");
+    isValid = false;
+  } else {
+    document.querySelector(".ck-editor").style.border = "";
+    document.querySelector("#editor-alert").classList.add("hidden");
   }
-  if(thumbnail.getAcceptedFiles().length==0){
-    productThumbnail.nextElementSibling.classList.remove('hidden')
-    productThumbnail.classList.add('!border-red-700')
-  }else{
-    productThumbnail.nextElementSibling.classList.add('hidden')
-    productThumbnail.classList.remove('!border-red-700')
+  if (thumbnail.getAcceptedFiles().length == 0) {
+    productThumbnail.nextElementSibling.classList.remove("hidden");
+    productThumbnail.classList.add("!border-red-700");
+    isValid = false;
+  } else {
+    productThumbnail.nextElementSibling.classList.add("hidden");
+    productThumbnail.classList.remove("!border-red-700");
   }
-  
+  return isValid;
 }
 
-cancelBtn.addEventListener('click' , ()=>{
-  if(productId) deleteProduct()
-    else{
-      showSwal('انصراف', 'آیا مطمئن هستید که می‌خواهید انصراف دهید ؟ اطلاعات ذخیره نخواهند شد.', 'warning', true, 'تایید')
-       .then(e=>{
-        if(e.isConfirmed) window.location.href='products.html'
-       })
+cancelBtn.addEventListener("click", () => {
+  if (productId) deleteProduct();
+  else {
+    showSwal(
+      "انصراف",
+      "آیا مطمئن هستید که می‌خواهید انصراف دهید ؟ اطلاعات ذخیره نخواهند شد.",
+      "warning",
+      true,
+      "تایید"
+    ).then((e) => {
+      if (e.isConfirmed) window.location.href = "products.html";
+    });
   }
-})
- function deleteProduct() {
-    showSwal('حذف محصول', 'آیا مطمئن هستید که می‌خواهید این محصول را حذف کنید؟ این عمل قابل بازگشت نیست.', 'warning', true, 'تایید')
-  .then(e=>{
-    if(e.isConfirmed){
-     fetch(`https://dummyjson.com/products/${productId}`, {
-        method: 'DELETE',
+});
+function deleteProduct() {
+  showSwal(
+    "حذف محصول",
+    "آیا مطمئن هستید که می‌خواهید این محصول را حذف کنید؟ این عمل قابل بازگشت نیست.",
+    "warning",
+    true,
+    "تایید"
+  ).then((e) => {
+    if (e.isConfirmed) {
+      fetch(`https://dummyjson.com/products/${productId}`, {
+        method: "DELETE",
       })
-      .then(res => res.json())
-      .then(showTost('success' , 'محصول با موفقیت حذف شد!') , window.location.href = 'product-add.html');
-   }
-  }  
-  )
+        .then((res) => res.json())
+        .then(
+          showTost("success", "محصول با موفقیت حذف شد!"),
+          setTimeout(() => {
+            (window.location.href = "product-add.html")
+          }, 1500)
+        );
+    }
+  });
 }
 
-function editProduct(){
+function submitProduct() {
+  showSwal(
+    productId ? "ویرایش محصول" : "اضافه کردن محول",
+    productId
+      ? "آیا میخواهید مشخصات این محصول را تغییر دهید؟"
+      : "آیا میخواهید این محصول را اضافه کنید؟",
+    "warning",
+    true,
+    "تایید"
+  ).then((data) => {
+    if (data.isConfirmed) {
+      let images = [];
+      gallery.getAcceptedFiles().forEach((img) => images.push(img.url));
+      let tags = [];
+      tagsContainer
+        .querySelectorAll("div span")
+        .forEach((elem) => tags.push(elem.textContent));
 
+      fetch(`https://dummyjson.com/products/${productId ? productId : "add"}`, {
+        method: productId ? "PUT" : "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          title: name.value,
+          sku: sku.value,
+          description: editorValue.getData(),
+          price: price.value,
+          stock: stock.value,
+          discountPercentage: discount.value,
+          shippingInformation: shipping.value,
+          warrantyInformation: warranty.value,
+          returnPolicy: returnElem.value,
+          category: category.value,
+          brand: brand.value,
+          tags: tags,
+          dimensions: {
+            width: width.value,
+            height: height.value,
+            depth: depth.value,
+          },
+          weight: weight.value,
+          thumbnail: thumbnail.getAcceptedFiles()[0].url,
+          images: images,
+        }),
+      })
+        .then((res) => res.json())
+        .then(
+          showTost('success' , productId? "مشخصات محصول آپدیت شد!" : "success", "محصول اضافه شد!" )
+        )
+        .catch((error) => {
+          console.log(error);
+          showTost("error", "مشکلی پیش آمد دوباره امتحان کنید!");
+        });
+    }
+  });
 }
