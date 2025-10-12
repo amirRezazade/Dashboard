b9a238b9415381d1ff00330dcf2fef5d1fecc72e
amir <amirrezazadeh.job@gmail.com>
2025-10-12 13:42:31 +0330

    add page styles

--- scripts/user-add.js
+++ scripts/user-add.js
@@ -5,3 +5,21 @@ import {
   showTost,
   addToShoppingCard,
 } from "./funcs.js";
+
+
+
+
+
+
+Dropzone.autoDiscover = false;
+const profile = new Dropzone("#user-photo", {
+  url: "#",
+  autoProcessQueue: false,
+  maxFiles: 1,
+  maxFilesize: 1,
+  acceptedFiles: "image/*",
+  dictDefaultMessage: " عکس کاربر را وارد کنید",
+  addRemoveLinks: true,
+  dictRemoveFile: "حذف تصویر",
+  dictFileTooBig: "حجم تصویر زیاد است (حداکثر: {{maxFilesize}}MB)",
+});
