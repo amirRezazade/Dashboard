36e204edbc11b7259ec464f56e25b68055a34775
amir <amirrezazadeh.job@gmail.com>
2025-10-19 21:02:13 +0330

    add page layout

--- scripts/profile.js
+++ scripts/profile.js
@@ -5,3 +5,15 @@ import {
   showTost,
   addToShoppingCard,
 } from "./funcs.js";
+
+const tabBtns = document.querySelector('#tab-btns')
+const tabWrapper = document.querySelector('#tab-container')
+
+
+tabBtns.addEventListener('click' , e=>{
+  if(e.target.nodeName==='BUTTON'){
+    tabBtns.querySelectorAll('button').forEach(elem=> elem.classList.remove('before:!w-full'))
+    e.target.classList.add('before:!w-full')
+    
+  }
+})
